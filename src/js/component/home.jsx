29904b4337b044abaf00d5c05b054d667804063d
Jose <83340481+josejesusjj@users.noreply.github.com>
2022-02-16 20:11:26 +0100

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InputTask from "./input.jsx";
+import Database from "./database.jsx";
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<InputTask />
+		</div>
+	);
+};
+
+export default Home;
